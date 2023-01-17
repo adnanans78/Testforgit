24a7a5e2e40bb64e0246ea2dfcab4562c198085a
Adnan Ansari <digicom.maison@gmail.com>
2023-01-18 01:47:29 +0500

    first Test

---
+++ src/main/java/com/TestforgitApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestforgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestforgitApplication.class, args);
+	}
+
+}
