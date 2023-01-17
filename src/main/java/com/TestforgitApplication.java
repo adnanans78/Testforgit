dd18e02c4e17a7b9e6e13b47ac098eaf77853eae
Adnan Ansari <digicom.maison@gmail.com>
2023-01-18 01:53:00 +0500

    second Test

--- src/main/java/com/TestforgitApplication.java
+++ src/main/java/com/TestforgitApplication.java
@@ -8,6 +8,12 @@ public class TestforgitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestforgitApplication.class, args);
+		TestforgitApplication.TestMsg("Wellcome to Git Test");
 	}
-
+	
+	public static void TestMsg(String s)
+	{
+		System.out.println(s);
+	}
 }
+   
