98af49e1f58d58c2eac680c2836274c5241e824e
Adnan Ansari <digicom.maison@gmail.com>
2023-01-18 14:04:58 +0500

    third commit

--- src/main/java/com/TestC.java
+++ src/main/java/com/TestC.java
@@ -2,16 +2,19 @@ package com;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestC {
 
-	@GetMapping
+	@RequestMapping("/Test")
 	public void Called()
 	{
 		
+		System.out.println("Testing here");
 	}
 	
 	
 }
+ 
