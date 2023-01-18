bf24da76af75fb8454515885693b0b7e5f58a567
Adnan Ansari <digicom.maison@gmail.com>
2023-01-18 15:43:56 +0500

    new file

---
+++ src/main/java/com/Counting.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Counting {
+	
+	public void Count()
+	{
+		System.out.println("Working with Counting");
+	}
+
+}
--- src/main/java/com/TestforgitApplication.java
+++ src/main/java/com/TestforgitApplication.java
@@ -1,15 +1,20 @@
 package com;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestforgitApplication {
 
+	@Autowired
+	 Counting ct;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestforgitApplication.class, args);
 		TestforgitApplication.TestMsg("Wellcome to Git Test");
 		System.out.println("Thirtd Commit");
+		//ct.Count();
 	}
 	  
 	public static void TestMsg(String s)
